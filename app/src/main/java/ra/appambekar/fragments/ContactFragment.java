18f568b2e4c72e7bef1027d9d1cc9f6ba9bf72a5
Rugved Ambekar <rugved.ambekar@gmail.com>
2016-06-05 15:56:54 -0400

    Updated contact page.

--- app/src/main/java/ra/appambekar/fragments/ContactFragment.java
+++ app/src/main/java/ra/appambekar/fragments/ContactFragment.java
@@ -9,13 +9,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.ScrollView;
+import android.widget.ImageView;
 
-import com.android.volley.toolbox.NetworkImageView;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
@@ -27,14 +25,11 @@ import org.greenrobot.eventbus.ThreadMode;
 import ra.appambekar.AmbekarApplication;
 import ra.appambekar.R;
 import ra.appambekar.helpers.FirebaseHelper;
-import ra.appambekar.helpers.VolleyHelper;
 import ra.appambekar.models.ContactCard;
 import ra.appambekar.models.events.LoginEvent;
 import ra.appambekar.models.events.NetworkEvent;
-import ra.appambekar.utilities.LayoutUtils;
 import ra.appambekar.views.ErrorItemView;
 import ra.appambekar.views.IconListItem;
-import ra.smarttextview.SmartTextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -43,18 +38,10 @@ public class ContactFragment extends Fragment {
 
     private static final String TAG = ContactFragment.class.getSimpleName();
 
-    private static final float ALPHA_BASE = 0.10f;
-
-    private ScrollView mScrollView;
-    private SmartTextView mThanksMsg;
-    private NetworkImageView mProfilePic;
     private IconListItem mLocation, mEmail, mPhone;
     private ErrorItemView mErrorView;
-    private View mPicFrame;
+    private ImageView mContactPic;
 
-    private ViewTreeObserver.OnScrollChangedListener mScrollListener;
-
-    private Typeface mThanksTypeface;
     private ContactCard mContactCard = null;
     private boolean mHasActiveConnection = false;
 
@@ -63,7 +50,6 @@ public class ContactFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mThanksTypeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Wolf.ttf");
         loadContactData();
     }
 
@@ -90,7 +76,7 @@ public class ContactFragment extends Fragment {
     }
 
     private void loadContactData() {
-        if (mHasActiveConnection = AmbekarApplication.hasActiveConnection()) {
+        if (mHasActiveConnection = AmbekarApplication.HasActiveConnection()) {
             FirebaseHelper.getInstance().getChildREF(FirebaseHelper.FireChild.Contact).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
@@ -112,13 +98,9 @@ public class ContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
 
-        mProfilePic = (NetworkImageView) rootView.findViewById(R.id.niv_profilePic);
-        mScrollView = (ScrollView) rootView.findViewById(R.id.scrollContainer_contact);
-
+        mContactPic = (ImageView) rootView.findViewById(R.id.iv_contactPic);
         mErrorView = (ErrorItemView) rootView.findViewById(R.id.eiv_contact);
-        mPicFrame = rootView.findViewById(R.id.fl_picFrame);
 
-        mThanksMsg = (SmartTextView) rootView.findViewById(R.id.tv_thanks);
         mLocation = (IconListItem) rootView.findViewById(R.id.ili_location);
         mEmail = (IconListItem) rootView.findViewById(R.id.ili_email);
         mPhone = (IconListItem) rootView.findViewById(R.id.ili_phone);
@@ -139,61 +121,9 @@ public class ContactFragment extends Fragment {
             }
         });
 
-        mThanksMsg.setAlpha(0);
-        mThanksMsg.setTypeface(mThanksTypeface);
-        mThanksMsg.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override public void onGlobalLayout() {
-                mThanksMsg.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                LayoutUtils.View.AdjustTextSizeForWidth(mThanksMsg, 0);
-            }
-        });
-
-        mProfilePic.getViewTreeObserver().addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener() {
-                    @Override public void onGlobalLayout() {
-                        mProfilePic.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                        mPicFrame.getLayoutParams().height = mProfilePic.getMeasuredHeight();
-                        mPicFrame.requestLayout();
-                    }
-                });
-
-        mScrollView.setOnTouchListener(new View.OnTouchListener() {
-            @Override public boolean onTouch(View v, MotionEvent event) {
-                // restrict all click and scroll features for view can child views
-                return !mHasActiveConnection || !FirebaseHelper.getInstance().isAuthenticated();
-            }
-        });
-        mScrollView.getViewTreeObserver().addOnScrollChangedListener(mScrollListener = new ViewTreeObserver.OnScrollChangedListener() {
-            float maxScrollY = 0, lastScrollY = 0;
-
-            @Override public void onScrollChanged() {
-                if (lastScrollY == mScrollView.getScrollY()) return;
-                if (maxScrollY == 0) maxScrollY = mScrollView.getChildAt(0).getHeight() - mScrollView.getHeight();
-                lastScrollY = mScrollView.getScrollY();
-
-                float percentScrolledY = Math.min(lastScrollY / maxScrollY, 1);
-                mPicFrame.setAlpha(1 - percentScrolledY);
-
-                if (percentScrolledY >= 0.85 && mThanksMsg.getAlpha() == 0) mThanksMsg.animate().alpha(0.75f).setDuration(500).start();
-                else if (percentScrolledY < 0.85 && mThanksMsg.getAlpha() > 0) mThanksMsg.animate().alpha(0).setDuration(500).start();
-
-                Log.d(TAG, String.format("onScrolled:: scrolledY=%f maxScrollY=%f alphaDelta=%f", lastScrollY, maxScrollY, (1 - ALPHA_BASE) * percentScrolledY));
-            }
-        });
-
         setContactInfo();
 
         return rootView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        if (mScrollView != null && mScrollListener != null) {
-            mScrollView.getViewTreeObserver().removeOnScrollChangedListener(mScrollListener);
-            mScrollListener = null;
-        }
-
-        super.onDestroyView();
     }
 
     private void setContactInfo() {
@@ -201,33 +131,30 @@ public class ContactFragment extends Fragment {
         if (mEmail != null && mContactCard != null) mEmail.switchContent(mContactCard.getEmail());
         if (mPhone != null && mContactCard != null) mPhone.switchContent(mContactCard.getPhone());
 
-        setErrorView();
 
-        if (mProfilePic != null && mContactCard != null) {
-            mProfilePic.postDelayed(new Runnable() {
-                @Override public void run() {
-                    mProfilePic.setImageUrl(mContactCard.getPicURL(), VolleyHelper.getInstance().getImageLoader());
-                }
-            }, 1000);
-        }
+        if (!setErrorView() && mContactPic != null) mContactPic.animate().alpha(1).setDuration(750).start();
     }
 
-    private void setErrorView() {
-        if (mErrorView == null) return;
+    private boolean setErrorView() {
+        if (mErrorView == null) return false;
 
         if (!mHasActiveConnection) {
             mErrorView.setForNoConnection(R.string.e_no_connection_info_contact, 0);
             mErrorView.setVisibility(View.VISIBLE);
+            return true;
 
         } else if (!FirebaseHelper.getInstance().isAuthenticated()) {
             mErrorView.setForNoAuthentication(R.string.e_no_auth_info_contact, 0);
             mErrorView.setVisibility(View.VISIBLE);
+            return true;
 
         } else if (mErrorView.getVisibility() == View.VISIBLE) {
             mErrorView.animate().alpha(0).setDuration(750).withEndAction(new Runnable() {
                 @Override public void run() { mErrorView.setVisibility(View.GONE); }
             }).start();
-
+            return false;
         }
+
+        return mErrorView.getVisibility() == View.VISIBLE;
     }
 }
